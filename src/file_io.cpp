87ce150b766d8570bfb1cef0e09fa579446671a8
rzdv <rzdv@rdwh.tech>
2026-05-10 03:19:59 +0300

    feat: implemented IO function for writing output file

--- src/file_io.cpp
+++ src/file_io.cpp
@@ -4,6 +4,7 @@
 
 #include "../include/file_io.h"
 #include <fstream>
+#include <map>
 
 
 std::vector<std::string> readFile(const std::string &path, std::vector<AppError> &errors) {
@@ -32,4 +33,36 @@ std::vector<std::string> readFile(const std::string &path, std::vector<AppError>
         lineNumber++;
     }
     return lines;
+}
+
+void writeResultsToFile(const std::vector<Span> &literals, const std::string &outputPath, std::vector<AppError> &errors) {
+    vector<string> insertionOrder;
+    map<string, vector<TextPos>> grouped;
+
+    for (const Span &span : literals) {
+        if (!grouped.contains(span.value)) {
+            insertionOrder.push_back(span.value);
+        }
+        grouped[span.value].push_back(span.pos);
+    }
+
+    ofstream out(outputPath);
+
+    if (!out.is_open()) {
+        errors.push_back(AppError::outputFailed(outputPath));
+        return;
+    }
+
+    for (const string &value : insertionOrder) {
+        const auto& positions = grouped[value];
+        out << "\"" << value << "\": [";
+
+        for (int i = 0; i < static_cast<int>(positions.size()); i++) {
+            if (i > 0) {
+                out << ", ";
+            }
+            out << "{" << positions[i].row << ", " << positions[i].col + 1 << "}";
+        }
+        out << "]\n";
+    }
 }
