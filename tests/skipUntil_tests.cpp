866b3f30b362955f1fc7377d88ebf4a109f0d06d
rzdv <rzdv@rdwh.tech>
2026-05-10 03:24:27 +0300

    test: added tests for skipUntil

--- tests/skipUntil_tests.cpp
+++ tests/skipUntil_tests.cpp
@@ -1,3 +1,62 @@
 //
 // Created by rzdv on 10.05.2026.
 //
+
+#include "gtest/gtest.h"
+#include "../include/parser.h"
+#include "test_utils.h"
+
+using std::vector;
+using std::string;
+
+TEST(TextCursorSkipUntilTests, SkipUntil_TokenOnSameLine_StopsAtTokenStart)
+{
+	const vector<string> text = { "abc*/def" };
+	TextCursor cursor = MakeCursor(text, 0, 0);
+
+	cursor.skipUntil("*/");
+
+	AssertTextPos(cursor.pos, 0, 3);
+}
+
+TEST(TextCursorSkipUntilTests, SkipUntil_TokenOnNextLine_StopsAtNextRow)
+{
+	const vector<string> text = { "abc", "*/def" };
+	TextCursor cursor = MakeCursor(text, 0, 0);
+
+	cursor.skipUntil("*/");
+
+	AssertTextPos(cursor.pos, 1, 0);
+}
+
+TEST(TextCursorSkipUntilTests, SkipUntil_MissingToken_LeavesCursorInvalidAfterFullScan)
+{
+	const vector<string> text = { "abc", "def" };
+	TextCursor cursor = MakeCursor(text, 0, 0);
+
+	cursor.skipUntil("*/");
+
+	ASSERT_FALSE(cursor.isValid());
+	AssertTextPos(cursor.pos, 2, 0);
+}
+
+TEST(TextCursorSkipUntilTests, SkipUntil_TokenAlreadyUnderCursor_DoesNotMove)
+{
+	const vector<string> text = { "*/abc" };
+	TextCursor cursor = MakeCursor(text, 0, 0);
+
+	cursor.skipUntil("*/");
+
+	AssertTextPos(cursor.pos, 0, 0);
+}
+
+TEST(TextCursorSkipUntilTests, SkipUntil_EmptyToken_DoesNotMove)
+{
+	const vector<string> text = { "abc" };
+	TextCursor cursor = MakeCursor(text, 0, 1);
+
+	cursor.skipUntil("");
+
+	AssertTextPos(cursor.pos, 0, 1);
+}
+
